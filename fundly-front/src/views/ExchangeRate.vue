b9c0821456f626bdd03e09d45449beac2364539a
Yeyeong99 <gongsam99@gmail.com>
2025-05-20 17:01:54 +0900

    [Feat] 환율 계산기 UI 구현

--- fundly-front/src/views/ExchangeRate.vue
+++ fundly-front/src/views/ExchangeRate.vue
@@ -1,11 +1,37 @@
 <template>
-<h1>환율</h1>
+  <div class="exchange-container">
+    <h2>환율</h2>
+    <div class="money">
+      <Form:get>
+        <InputNumber id="won" v-model="input"></InputNumber>
+        <label for="won">₩</label>
+      </Form:get>
+    </div>
+    <div class="money">
+      <Form:get>
+        <InputNumber id="dollar" v-model="output"></InputNumber>
+        <label for="dollar">$</label>
+      </Form:get>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import InputNumber from 'primevue/inputnumber';
 
+import { Form } from '@primevue/forms'
+
+import { ref } from 'vue'
+
+const input = ref(0)
+const output = ref(0)
 </script>
 
 <style scoped>
-
+label {
+  padding-left: 1rem;
+}
+.money {
+  margin-bottom: 1rem;
+} 
 </style>
