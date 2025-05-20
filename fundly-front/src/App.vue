501e86426f63d28caaac805557496ea8d4af6a69
Yeyeong99 <gongsam99@gmail.com>
2025-05-20 11:43:27 +0900

    [Feat] Set Theme Preset

--- fundly-front/src/App.vue
+++ fundly-front/src/App.vue
@@ -2,29 +2,29 @@
   <div class="container">
     <div class="side-bar">
       <div class="basic-menu">
-        <h4>기본 메뉴</h4>
+        <h3>기본 메뉴</h3>
         <div class="basic-menu-list">
-          <RouterLink to="/"><Button label="목표 확인" variant="text" /></RouterLink>
-          <RouterLink to="/setgoal"><Button label="목표 추가하기" variant="text" /></RouterLink>
-          <RouterLink to="/checkproducts"><Button label="금융 상품 확인" variant="text" /></RouterLink>
-          <RouterLink to="/recommendproducts"><Button label="금융 상품 추천" variant="text" /></RouterLink>
+          <RouterLink to="/"><Button label="목표 확인" text /></RouterLink>
+          <RouterLink to="/setgoal"><Button label="목표 추가하기" text /></RouterLink>
+          <RouterLink to="/checkproducts"><Button label="금융 상품 확인" text /></RouterLink>
+          <RouterLink to="/recommendproducts"><Button label="금융 상품 추천" text /></RouterLink>
           <RouterLink to="/searchbank"
-            ><Button label="주변 은행 / ATM 찾기" variant="text"
+            ><Button label="주변 은행 / ATM 찾기" text
           /></RouterLink>
-          <RouterLink to="/community"><Button label="커뮤니티" variant="text" /></RouterLink>
-          <RouterLink to="/exchangerate"><Button label="환율 계산" variant="text" /></RouterLink>
+          <RouterLink to="/community"><Button label="커뮤니티" text /></RouterLink>
+          <RouterLink to="/exchangerate"><Button label="환율 계산" text /></RouterLink>
         </div>
       </div>
       <div class="my-page">
-        <h4>마이 페이지</h4>
+        <h3>마이 페이지</h3>
         <div class="my-page-list">
           <RouterLink to="/edit/personalinfo"
-            ><Button label="개인 정보 수정" variant="text"
+            ><Button label="개인 정보 수정" text
           /></RouterLink>
           <RouterLink to="/likeproducts"
-            ><Button label="찜한 상품 확인" variant="text"
+            ><Button label="찜한 상품 확인" text
           /></RouterLink>
-          <RouterLink to="/qna"><Button label="자주 묻는 질문" variant="text" /></RouterLink>
+          <RouterLink to="/qna"><Button label="자주 묻는 질문" text /></RouterLink>
         </div>
       </div>
     </div>
@@ -38,10 +38,14 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <style scoped>
+button {
+  
+}
 .container {
-  height: 100%;
+  width: 95%;
+  height: 95%;
   display: flex;
-  border: 0.3px solid;
+  border: 0.3px solid var(--p-gray-300);
   border-radius: 30px;
 }
 
@@ -50,7 +54,7 @@ import { RouterLink, RouterView } from 'vue-router'
   flex-direction: column;
   justify-content: center;
   margin-left: 2rem;
-  border-right: 0.3px solid;
+  border-right: 0.3px solid var(--p-gray-300);
   padding-right: 2rem;
 }
 
@@ -62,5 +66,6 @@ import { RouterLink, RouterView } from 'vue-router'
 }
 
 .basic-menu-list {
+  margin-bottom: 4rem;
 }
 </style>
--- fundly-front/src/main.js
+++ fundly-front/src/main.js
@@ -1,29 +1,64 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Aura from '@primeuix/themes/aura'
-import PrimeVue from 'primevue/config'
+import { definePreset } from '@primeuix/themes'
+import Lara from '@primeuix/themes/lara'
 
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
 import router from './router'
 import Button from 'primevue/button'
 
-// const MyPreset = definePreset(Aura, {
-//     semantic: {
-//         primary: {
-//             50: '{indigo.50}',
-//             100: '{indigo.100}',
-//             200: '{indigo.200}',
-//             300: '{indigo.300}',
-//             400: '{indigo.400}',
-//             500: '{indigo.500}',
-//             600: '{indigo.600}',
-//             700: '{indigo.700}',
-//             800: '{indigo.800}',
-//             900: '{indigo.900}',
-//             950: '{indigo.950}'
-//         }
-//     }
-// });
+const MyPreset = definePreset(Lara, {
+  semantic: {
+    primary: {
+      50: '{slate.50}',
+      100: '{slate.100}',
+      200: '{slate.200}',
+      300: '{slate.300}',
+      400: '{slate.400}',
+      500: '{slate.500}',
+      600: '{slate.600}',
+      700: '{slate.700}',
+      800: '{slate.800}',
+      900: '{slate.900}',
+      950: '{slate.950}',
+    },
+  },
+  components: {
+    button: {
+      root: {
+        primary: {
+          background: '{slate.900}',
+          color: 'white',
+          border: '{slate.900}',
+          hoverBg: '{slate.800}',
+          focusShadow: '0 0 0 2px #ffffff, 0 0 0 4px {slate.700}',
+        },
+        label: {
+          fontWeight: 400,
+        },
+      },
+      text: {
+        primary: {
+          color: '{slate.900}',
+          fontWeight: 100,
+
+          hoverBg: '{slate.100}',
+          focusShadow: '0 0 0 2px #ffffff, 0 0 0 4px {slate.700}',
+        },
+      },
+      outlined: {
+        primary: {
+          fontWeight: 100,
+          color: '{slate.900}',
+          border: '{slate.500}',
+          hoverBg: '{slate.100}',
+          focusShadow: '0 0 0 2px #ffffff, 0 0 0 4px {slate.700}',
+        },
+      },
+    },
+  },
+})
 
 const app = createApp(App)
 
@@ -31,7 +66,7 @@ app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: MyPreset,
   },
 })
 
