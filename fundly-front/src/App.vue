4e4f9c168841ff4a973970f951658d6b6a9a0422
Yeyeong99 <gongsam99@gmail.com>
2025-05-20 09:24:13 +0900

    [Feat] Router 설정

--- fundly-front/src/App.vue
+++ fundly-front/src/App.vue
@@ -4,31 +4,37 @@
       <div class="basic-menu">
         <h4>기본 메뉴</h4>
         <div class="basic-menu-list">
-          <RouterLink to="/"><Button label="목표 확인" variant="text"/></RouterLink>
-          <RouterLink to="/"><Button label="목표 추가하기" variant="text"/></RouterLink>
-          <RouterLink to="/"><Button label="금융 상품 확인" variant="text"/></RouterLink>
-          <RouterLink to="/"><Button label="금융 상품 추천" variant="text"/></RouterLink>
-          <RouterLink to="/"><Button label="주변 은행 / ATM 찾기" variant="text"/></RouterLink>
-          <RouterLink to="/"><Button label="커뮤니티" variant="text"/></RouterLink>
-          <RouterLink to="/"><Button label="환율 계산" variant="text"/></RouterLink>
+          <RouterLink to="/"><Button label="목표 확인" variant="text" /></RouterLink>
+          <RouterLink to="/setgoal"><Button label="목표 추가하기" variant="text" /></RouterLink>
+          <RouterLink to="/checkproducts"><Button label="금융 상품 확인" variant="text" /></RouterLink>
+          <RouterLink to="/recommendproducts"><Button label="금융 상품 추천" variant="text" /></RouterLink>
+          <RouterLink to="/searchbank"
+            ><Button label="주변 은행 / ATM 찾기" variant="text"
+          /></RouterLink>
+          <RouterLink to="/community"><Button label="커뮤니티" variant="text" /></RouterLink>
+          <RouterLink to="/exchangerate"><Button label="환율 계산" variant="text" /></RouterLink>
         </div>
       </div>
       <div class="my-page">
         <h4>마이 페이지</h4>
         <div class="my-page-list">
-          <RouterLink to="/"><Button label="개인 정보 수정" variant="text"/></RouterLink>
-          <RouterLink to="/"><Button label="찜한 상품 확인" variant="text"/></RouterLink>
-          <RouterLink to="/"><Button label="자주 묻는 질문" variant="text"/></RouterLink>
+          <RouterLink to="/edit/personalinfo"
+            ><Button label="개인 정보 수정" variant="text"
+          /></RouterLink>
+          <RouterLink to="/likeproducts"
+            ><Button label="찜한 상품 확인" variant="text"
+          /></RouterLink>
+          <RouterLink to="/qna"><Button label="자주 묻는 질문" variant="text" /></RouterLink>
         </div>
       </div>
     </div>
 
-    <div class="main"></div>
+    <div class="main"><RouterView /></div>
   </div>
 </template>
 
 <script setup>
-import Button from 'primevue/button'
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <style scoped>
---
+++ fundly-front/src/views/CheckProducts.vue
@@ -0,0 +1,11 @@
+<template>
+<h1>금융 상품 확인</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
