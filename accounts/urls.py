7429b7d2755b1e14004e24391df15e9e1bf7ca00
surin <tjdtnfls1128@gmail.com>
2025-05-21 15:08:35 +0900

    [Feat] 닉네임/비밀번호 수정, 프로필 조회 기능 #10

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
-
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 from . import views
 
 urlpatterns = [
     path('auth/signup/', views.signup),    # 회원가입
     path('auth/login/', views.login),    # 로그인
-    path('auth/logout/', views.logout),    # 로그아웃
     path('auth/<str:provider>/social-login/', views.social_login),    # 소셜 로그인
+    path('auth/social-logout/', views.social_logout),    # 소셜 로그아웃
     path('auth/<str:provider>/callback/', views.callback),    # 콜백 함수
-    # path('user/', views.profile),    # 회원 정보
-    # path('user/nickname/', views.nickname),    # 닉네임
+    path('user/profile/', views.profile),    # 회원 정보
+    path('user/nickname/', views.set_nickname),    # 닉네임
+    path('user/change-password/', views.change_password),    # 비밀번호 수정
 ]
